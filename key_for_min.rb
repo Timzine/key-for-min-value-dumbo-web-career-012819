012a8163e858948f0fe0e18d023ba9fb853bd6d5
Lemrabt Boudadya <lboudadya@gmail.com>
2019-01-12 01:54:06 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,14 +3,15 @@
 
 def key_for_min_value(name_hash)
   lowest_key = nil 
-  lowest_value = nil
+  lowest_value = nil 
   name_hash.each do |k, v|
-    if v < lowest_value
+    if lowest_value == nil || v < lowest_value
       lowest_value = v 
       lowest_key = k 
-    end
-  end
-   lowest_key
+    end 
+  end 
+  lowest_key
+
       
 
 end
